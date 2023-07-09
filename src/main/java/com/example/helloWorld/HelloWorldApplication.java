59bb662bd3f8b2558752ee7a6859c254461bd9bc
hgzUpUpUp <hgz_1994@163.com>
2023-07-09 23:05:24 +0800

    version 5

--- src/main/java/com/example/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/HelloWorldApplication.java
@@ -9,6 +9,8 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 		System.out.printlin("version2");
+		//hgzUpUpUp修改的代码
+		System.out.printlin("version edit by hgzUpUpUp");
 	}
 
 }
