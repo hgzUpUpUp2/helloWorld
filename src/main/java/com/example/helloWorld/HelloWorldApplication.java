882b9d9cec57999a479ce7e4047969385690dd89
hgzUpUpUp <hgz_1994@163.com>
2023-07-10 11:53:19 +0800

    使用idea 进行git管理 version1

--- src/main/java/com/example/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/HelloWorldApplication.java
@@ -8,14 +8,8 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.printlin("version2");
-		System.out.printlin("version edit by hgzUpUpUp");
-		//hgzUpUpUp2修改的代码
-		System.out.printlin("version3 edit by hgzUpUpUp2");
-		//hgzUpUpUp2修改的代码
-		System.out.printlin("version4 edit by hgzUpUpUp2");
-		//hgzUpUpUp修改的代码
-		System.out.printlin("version5 edit by hgzUpUpUp");
+		System.out.println("version2");
+		System.out.println("IDEA helloWorld version1");
 	}
 
 }
